b6ed74605ef778b9d3aae43cd29e95f1ae4d06e8
FPierre <pflauder@gmail.com>
2016-01-07 22:02:03 +0100

    Création de la route, Controller et Views pour tester la feature de delay

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'test/delay'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'test#delay'
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
