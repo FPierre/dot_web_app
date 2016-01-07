fc8e646e388c8d244710589879bb842a5837b6d5
FPierre <pflauder@gmail.com>
2016-01-07 22:39:36 +0100

    POC pour Websockets depuis un Controller

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,4 +1,10 @@
 class TestController < ApplicationController
+  def index
+  end
+
   def delay
+    ActionCable.server.broadcast 'notification_channel', notification: 'test'
+
+    head :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'test/delay'
+  get 'test_delay', to: 'test#delay'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'test#index'
 
-  root 'test#delay'
-
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
+  mount ActionCable.server => '/cable'
 end
