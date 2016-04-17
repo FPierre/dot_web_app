60b193d64e987b6f8d3ac3c7f30a1bf90eb4264c
FPierre <pflauder@gmail.com>
2016-04-17 14:18:35 +0200

    Continuation des tests de documentation

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,6 +1,10 @@
 class RoutesController < ApplicationController
   acts_as_token_authentication_handler_for User
 
+  api :GET, '/resize/zone/:zone/size/:size', 'Resize the zone'
+  description 'Get a document if user has the rights'
+  param :zone, [:one, :two], desc: 'Zone ID', required: true
+  param :size, [:full, :half], desc: 'Size', required: true
   def show
     if params[:from].present? && params[:to].present?
       from = Geocoder.search params[:from]
