ec5889e5c06aa48ba4d060f0c39e6707e6a745dd
FPierre <pflauder@gmail.com>
2016-04-17 14:02:08 +0200

    Ajout de MaterializeCSS sur la documentation

---
+++ app/assets/javascripts/apipie.js
@@ -0,0 +1,10 @@
+//= require jquery2
+//= require jquery_ujs
+//= require turbolinks
+//= require materialize-sprockets
+//
+//= require_tree .
+
+$(document).on('ready page:load', function() {
+
+});
