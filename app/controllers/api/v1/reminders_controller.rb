31619dd0ef5dc3f8a4b1ec9b74b08fac8e7d112a
FPierre <pflauder@gmail.com>
2016-04-19 22:26:05 +0200

    URL de création d'un Reminder et désactivation de la protection CSRF

--- app/controllers/api/v1/reminders_controller.rb
+++ app/controllers/api/v1/reminders_controller.rb
@@ -3,63 +3,26 @@ module Api
     class RemindersController < ApplicationController
       acts_as_token_authentication_handler_for User, fallback: :none
 
-      before_action :set_reminder, only: [:show, :edit, :update, :destroy]
-
-      def index
-        @reminders = Reminder.all
-      end
-
-      def show
-      end
-
-      def new
-        @reminder = Reminder.new
-      end
-
-      def edit
-      end
-
+      api :POST, '/reminders', 'Create a Reminder'
+      description 'Request Google API to get the car path between the city "from" and the other city "to"'
+      error code: 422, desc: 'Unprocessable entity'
+      example "curl -X POST http://localhost:3000/api/v1/reminders -H \"Content-type: application/json\" -d '{ \"title\": \"title\", \"content\": \"content\" }'"
+      meta client: [:android_application], status: :pending
+      param :title, String, desc: 'Reminder title', required: true
+      param :content, String, desc: 'Reminder content', required: true
       def create
         @reminder = Reminder.new(reminder_params)
 
-        respond_to do |format|
-          if @reminder.save
-            format.html { redirect_to @reminder, notice: 'Reminder was successfully created.' }
-            format.json { render :show, status: :created, location: @reminder }
-          else
-            format.html { render :new }
-            format.json { render json: @reminder.errors, status: :unprocessable_entity }
-          end
-        end
-      end
-
-      def update
-        respond_to do |format|
-          if @reminder.update(reminder_params)
-            format.html { redirect_to @reminder, notice: 'Reminder was successfully updated.' }
-            format.json { render :show, status: :ok, location: @reminder }
-          else
-            format.html { render :edit }
-            format.json { render json: @reminder.errors, status: :unprocessable_entity }
-          end
-        end
-      end
-
-      def destroy
-        @reminder.destroy
-        respond_to do |format|
-          format.html { redirect_to reminders_url, notice: 'Reminder was successfully destroyed.' }
-          format.json { head :no_content }
+        if @reminder.save
+          render json: @reminder, status: :created
+        else
+          render json: @reminder.errors, status: :unprocessable_entity
         end
       end
 
       private
-        def set_reminder
-          @reminder = Reminder.find(params[:id])
-        end
-
         def reminder_params
-          params.require(:reminder).permit(:title, :content, :user_id)
+          params.require(:reminder).permit(:title, :content)
         end
     end
   end
