8530af122834e80cac414092bcd982f89ee79d9c
FPierre <pflauder@gmail.com>
2016-04-20 22:47:10 +0200

    Avatar de User de test

--- app/assets/javascripts/views/settings/index.js
+++ app/assets/javascripts/views/settings/index.js
@@ -1,6 +1,8 @@
 $(document).on('ready page:load', function() {
   if ($('body.settings.index').length) {
+
     // Suppression de l'ombre sur le nav si tab juste en dessous
+
     var $nav = $('nav'),
         $tabs = $('main .tabs');
 
@@ -8,5 +10,9 @@ $(document).on('ready page:load', function() {
       $tabs.removeClass('z-depth-1')
            .css('box-shadow', '0 2px 3px 0 rgba(0, 0, 0, 0.21), 0 0 0 0 rgba(0, 0, 0, 0)');
     }
+
+    // Sidebar
+
+    $('.sidebar').sideNav();
   }
 });
