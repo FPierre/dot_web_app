88f926ea31efdc00fde360232ee5ac642a227812
FPierre <pflauder@gmail.com>
2016-04-23 21:43:45 +0200

    Ajustements sur le Create de Reminders

--- app/controllers/api/v1/reminders_controller.rb
+++ app/controllers/api/v1/reminders_controller.rb
@@ -1,28 +1,26 @@
 module Api
   module V1
     class RemindersController < ApplicationController
-      acts_as_token_authentication_handler_for User, fallback: :none
-
       api :POST, '/reminders', 'Create a Reminder'
-      description 'Request Google API to get the car path between the city "from" and the other city "to"'
+      # description ''
       error code: 422, desc: 'Unprocessable entity'
       example "curl -X POST http://localhost:3000/api/v1/reminders -H \"Content-type: application/json\" -d '{ \"title\": \"title\", \"content\": \"content\" }'"
       meta client: [:android_application], status: :pending
       param :title, String, desc: 'Reminder title', required: true
       param :content, String, desc: 'Reminder content', required: true
       def create
-        @reminder = Reminder.new(reminder_params)
+        reminder = Reminder.new reminder_params
 
-        if @reminder.save
-          render json: @reminder, status: :created
+        if reminder.save
+          render json: reminder, status: :created
         else
-          render json: @reminder.errors, status: :unprocessable_entity
+          render json: reminder.errors, status: :unprocessable_entity
         end
       end
 
       private
         def reminder_params
-          params.require(:reminder).permit(:title, :content)
+          params.permit(:title, :content)
         end
     end
   end
