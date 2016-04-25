3ab41e3bf2ccafe78aa44226d36448bc6790552d
FPierre <pflauder@gmail.com>
2016-04-25 17:28:52 +0200

    Toutes les méthodes sont passées en 'pending' par défaut

--- app/controllers/api/v1/reminders_controller.rb
+++ app/controllers/api/v1/reminders_controller.rb
@@ -6,8 +6,7 @@ module Api
       api :POST, '/reminders', 'Create a Reminder'
       # description ''
       error code: 422, desc: 'Unprocessable entity'
-      meta clients: [:android_application], status: :ok
-      meta status: :ok
+      meta clients: [:android_application], status: :pending
       param :title, String, desc: 'Reminder title', required: true
       param :content, String, desc: 'Reminder content', required: true
       def create
