abb6a390f8487bef0ec4f5de68e9be12e4fcb914
FPierre <pflauder@gmail.com>
2016-04-25 18:24:31 +0200

    Ajout de firstname et lastname au Model User (obligatoires)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322155822) do
+ActiveRecord::Schema.define(version: 20160425162147) do
 
   create_table "reminders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "title"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20160322155822) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
+    t.string   "firstname",                              null: false
+    t.string   "lastname",                               null: false
   end
 
   add_index "users", ["approved"], name: "index_users_on_approved", using: :btree
