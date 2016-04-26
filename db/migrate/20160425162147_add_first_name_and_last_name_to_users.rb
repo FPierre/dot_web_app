94e2a5d9221d546cb1b049f11044d8e576437d4a
FPierre <pflauder@gmail.com>
2016-04-26 22:58:58 +0200

    Réinitialisation de la DB

--- db/migrate/20160425162147_add_first_name_and_last_name_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddFirstNameAndLastNameToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :firstname, :string, null: false
-    add_column :users, :lastname, :string, null: false
-  end
-end
