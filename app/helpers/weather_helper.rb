834a2ff5eb45f91d1e8ccabf1e4e1d01f4d498fd
FPierre <pflauder@gmail.com>
2016-06-19 16:19:42 +0200

    Helper d'icônes pour la météo

---
+++ app/helpers/weather_helper.rb
@@ -0,0 +1,24 @@
+module WeatherHelper
+  def weather_icons_codes
+    {
+      '01d' => 'wi-day-sunny',
+      '02d' => 'wi-day-cloudy',
+      '03d' => 'wi-cloud',
+      '04d' => 'wi-cloudy',
+      '09d' => 'wi-showers',
+      '10d' => 'wi-day-rain',
+      '11d' => 'wi-storm-showers',
+      '13d' => 'wi-snow',
+      '50d' => 'wi-fog',
+      '01n' => 'wi-night-clear',
+      '02n' => 'wi-night-alt-cloudy',
+      '03n' => 'wi-cloud',
+      '04n' => 'wi-cloudy',
+      '09n' => 'wi-showers',
+      '10n' => 'wi-night-rain',
+      '11n' => 'wi-storm-showers',
+      '13n' => 'wi-snow',
+      '50n' => 'wi-fog'
+    }
+  end
+end
