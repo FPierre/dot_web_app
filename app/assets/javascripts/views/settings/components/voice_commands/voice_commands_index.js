a5c4abf8f9f57f4dad6cc914c807d36f1ba1ecbf
FPierre <pflauder@gmail.com>
2016-06-19 16:15:43 +0200

    Suppression de code inadapté

--- app/assets/javascripts/views/settings/components/voice_commands/voice_commands_index.js
+++ app/assets/javascripts/views/settings/components/voice_commands/voice_commands_index.js
@@ -7,41 +7,5 @@ Vue.component('voice-commands-index', {
       </div>\
     </li>\
     <voice-command-show v-for="voiceCommand in voiceCommands" :voice-command="voiceCommand"></voice-command-show>\
-  </ul>',
-  data: function () {
-    return {
-      pressedUsersIds: [],
-      userToEdit: null
-    }
-  },
-  computed: {
-    pressedUsersIdsTitle: function () {
-      var plural = (this.pressedUsersIds.length > 1) ? 's' : ''
-
-      return this.pressedUsersIds.length + ' utilisateur' + plural + ' sélectionné' + plural
-    }
-  },
-  methods: {
-    delete: function () {
-      if (this.pressedUsersIds.length > 0) {
-        for (var i = 0; i < this.pressedUsersIds.length; i++) {
-          this.$http({ url: 'users/' + this.pressedUsersIds[i], method: 'DELETE' }).then(function (response) {
-            // console.log(response)
-            // console.log(response.data.id)
-
-            this.users = this.users.filter(function (currentUser) {
-              return currentUser.id != response.data.id
-            })
-
-            this.pressedUsersIds = this.pressedUsersIds.filter(function (currentUserId) {
-              return currentUserId != response.data.id
-            })
-          }, function (response) {
-            console.log('catch')
-            console.log(response)
-          })
-        }
-      }
-    }
-  }
+  </ul>'
 })
