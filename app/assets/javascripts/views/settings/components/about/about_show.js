2f48f24a21bcfbb1a6ffb1e9fefbe6ecca637a58
FPierre <pflauder@gmail.com>
2016-06-19 16:19:19 +0200

    Ajout des crédits du projet

---
+++ app/assets/javascripts/views/settings/components/about/about_show.js
@@ -0,0 +1,29 @@
+Vue.component('about-show', {
+  template: '<ul class="collection with-header">\
+    <li class="collection-header">\
+      <div class="row" style="margin-bottom: 0;">\
+        <h4>Crédits</h4>\
+      </div>\
+    </li>\
+    <li class="collection-item">\
+      <span class="title">Entreprise : itnovem.</span>\
+      <p>2016</p>\
+    </li>\
+    <li class="collection-item">\
+      <span class="title">Développeur SARAH : Thierry Allard saint alban</span>\
+      <p><a href="https://github.com/tasa95">GitHub</a></p>\
+    </li>\
+    <li class="collection-item">\
+      <span class="title">Développeur application Android : Nicolas Journaud</span>\
+      <p><a href="https://github.com/save92">GitHub</a></p>\
+    </li>\
+    <li class="collection-item">\
+      <span class="title">Développeur API & Application Web : Pierre Flauder</span>\
+      <p><a href="https://github.com/FPierre">GitHub</a></p>\
+    </li>\
+    <li class="collection-item">\
+      <span class="title">Graphiste logo : David Lock</span>\
+      <p>lock_yeton@live.fr</p>\
+    </li>\
+  </ul>'
+})
