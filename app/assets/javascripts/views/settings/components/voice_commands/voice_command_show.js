da16173cfe307d4470d461b9f7caf2e11853ed8b
FPierre <pflauder@gmail.com>
2016-06-21 16:35:22 +0200

    Affichage des commandes vocales plus précis

--- app/assets/javascripts/views/settings/components/voice_commands/voice_command_show.js
+++ app/assets/javascripts/views/settings/components/voice_commands/voice_command_show.js
@@ -2,6 +2,6 @@ Vue.component('voice-command-show', {
   props: ['voiceCommand'],
   template: '<li class="collection-item">\
     <span class="title">{{ voiceCommand.name }}</span>\
-    <p>{{ voiceCommand.description }}</p>\
+    <p v-for="desc in voiceCommand.description">{{ desc }}</p>\
   </li>'
 })
