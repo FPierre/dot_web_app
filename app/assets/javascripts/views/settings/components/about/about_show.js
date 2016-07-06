0d0ac52831e7a3c4da32bd37eaf4e2d25ba4dd8c
FPierre <pflauder@gmail.com>
2016-07-06 20:10:46 +0200

    Correction nom de Thierry

--- app/assets/javascripts/views/settings/components/about/about_show.js
+++ app/assets/javascripts/views/settings/components/about/about_show.js
@@ -10,7 +10,7 @@ Vue.component('about-show', {
       <p>2016</p>\
     </li>\
     <li class="collection-item">\
-      <span class="title">Développeur SARAH : Thierry Allard saint alban</span>\
+      <span class="title">Développeur SARAH : Thierry Allard saint albin</span>\
       <p><a href="https://github.com/tasa95">GitHub</a></p>\
     </li>\
     <li class="collection-item">\
