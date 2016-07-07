841592f578c9352d8668d93c4d960dd43184b9b2
FPierre <pflauder@gmail.com>
2016-07-07 20:18:45 +0200

    Enregistrement : gestion des erreurs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,21 @@
 module ApplicationHelper
+  def pretty_errors errors
+    if errors.is_a? Hash
+      pretty_msg = '<ul>'
 
+      errors.each do |attr, errors_msg|
+        msg = if errors_msg.is_a? Array
+          errors_msg.to_sentence
+        else
+          errors_msg.first
+        end
+
+        pretty_msg << "<li>#{attr} : #{msg}</li>"
+      end
+
+      pretty_msg << '</ul>'
+    else
+      errors
+    end
+  end
 end
